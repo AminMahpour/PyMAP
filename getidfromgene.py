35a12b95456a917b9f1ae424a3bd9ec184820dc4
Amin Mahpour <aminmahpour@Amins-MacBook-Pro.local>
2015-10-09 23:10:39 -0400

    + Added an executable to retrieve probe id from gene name.

---
+++ getidfromgene.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import argparse
+import Core
+import Annotation
+import os
+
+
+def check_file(filename):
+    """
+    Chech for the existance of the input filename.
+    :param filename: filename to check.
+    :return:
+    """
+    return os.path.exists( os.path.abspath(filename))
+
+# USAGE : ./convertbed.py -file Data/GSE42308.txt -out gello.bed -gene DENR
+parser = argparse.ArgumentParser()
+parser.add_argument("-gene", help="Name of gene that probe id will be returned for")
+parser.add_argument("-out", help="get output with [y]")
+
+args = vars(parser.parse_args())
+
+
+
+annotations = Annotation.Annotator()
+probes = annotations.get_probes_from_gene(args["gene"])
+
+
+for i in probes:
+    print(i.id)
+
+if args["out"] == "y" or args["out"] == "Y":
+    out_file = open("%s-probes.txt" % args["gene"], mode="w")
+    for i in probes:
+        out_file.write("%s\n"%i.id)
+    out_file.close()
+
+
+if len(probes) == 0 :
+    print("Your gene did not return a valid probe id")
