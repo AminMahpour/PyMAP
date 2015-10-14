9c1b011bc12638d8552e342b926100c1c28e052d
Amin Mahpour <aminmahpour@Amins-MacBook-Pro.local>
2015-10-13 23:47:50 -0400

    updated documentations.

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,8 @@ import shlex
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-import Annotation
 
 # -- General configuration ------------------------------------------------
 
